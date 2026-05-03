de168f7f11f98d28242820a49394e4264230a925
edward <edwardedo603@gmail.com>
2026-05-03 23:42:49 +0700

    refactor(prisma): streamline PrismaClient initialization and remove global variable usage
    - Removed the global variable for PrismaClient and simplified its initialization.
    - Added dotenv configuration for environment variable management.

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,19 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
 import { PrismaNeon } from "@prisma/adapter-neon";
-
-declare global {
-  var __prisma__: PrismaClient | undefined;
-}
+import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL!,
 });
 
-export const prisma: PrismaClient =
-  globalThis.__prisma__ ??
-  new PrismaClient({
-    adapter,
-    log: ["error", "warn"],
-  });
-
-if (process.env.NODE_ENV !== "production") globalThis.__prisma__ = prisma;
+export const prisma = new PrismaClient({ adapter });
